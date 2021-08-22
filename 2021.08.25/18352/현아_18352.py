0bef1056ee939af73dad26b9a13e2c748aa8f39b
HyunA-Kim (Mina) <65345381+kha-github@users.noreply.github.com>
2021-08-23 00:27:52 +0900

    2021.08.22

--- 2021.08.25/18352/현아_18352.py
+++ 2021.08.25/18352/현아_18352.py
@@ -1,1 +1,34 @@
+from collections import deque
 
+n, m, k, x = map(int, input().split(" "))
+
+mylist = [[-1] for _ in range(n+1)]
+
+for _ in range(m):
+  a, b = map(int, input().split(" "))
+  mylist[a].append(b)
+
+visit = [0]*(n+1)
+
+def BFS(start):
+
+  myq = deque([start, ])
+  visit[start] = 0
+
+  while myq:
+    cur = myq.popleft()
+    for idx in range(1, n+1):
+      if visit[idx] == 0 and idx in mylist[cur] and visit[cur] <= k:
+        myq.append(idx)
+        visit[idx] = visit[cur] + 1
+
+BFS(x)
+
+has_result = False
+for idx in range(1, len(visit)):
+  if visit[idx] == k and idx != x:
+    print(idx)
+    has_result = True
+
+if not(has_result):
+  print("-1")
