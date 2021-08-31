e2769ca01c7a9e1d32d322f604d8a0da52e07273
JINWOO JEONG <62453929+jeongjinwoo96@users.noreply.github.com>
2021-08-31 12:36:12 +0900

    Create 진우_1931.py

---
+++ 2021.09.01/1931/진우_1931.py
@@ -0,0 +1,13 @@
+N = int(input())
+con = []
+for i in range(N):
+    s,f = map(int,input().split())
+    con.append((s,f))
+sorted(con,key=lambda x:(x[0],x[1]))
+f = con[0][1]
+ans = 1
+for i in range(1,N):
+    if con[i][0]>=f:
+        ans +=1
+        f = con[i][1]
+print(ans)
