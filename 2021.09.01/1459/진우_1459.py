00a30b6cd5e0251cba7ffe8296af38c0dbb12b5a
JINWOO JEONG <62453929+jeongjinwoo96@users.noreply.github.com>
2021-09-01 02:38:06 +0900

    통과

--- 2021.09.01/1459/진우_1459.py
+++ 2021.09.01/1459/진우_1459.py
@@ -1,5 +1,26 @@
 x,y,w,s = map(int, input().split())
-if x==0 or y==0 or w*2 < s:
+m = min(x,y)
+M = max(x,y)
+if x==0 or y==0:
+    if M % 2 ==0:
+        if w < s:
+            print(M * w)
+        else:
+            print(M * s)
+    else:
+        if (M-1)*s + w < M*w:
+            print((M-1)*s + w)
+        else:
+            print(M*w)
+elif w*2 < s:
     print((x+y)*w)
+elif (x+y - 2*min(x,y)) % 2 == 0:
+    if w<s:
+        print(min(x,y)*s + (x+y-2*min(x,y))*w)
+    else:
+        print(min(x,y)*s + (x+y-2*min(x,y))*s)
 else:
-    print(min(x,y)*s + (x+y-2*min(x,y))*w)
+    if (x+y-2*min(x,y)-1)*s + w < (x+y-2*min(x,y))*w:
+        print(min(x,y)*s + (x+y-2*min(x,y)-1)*s + w)
+    else:
+        print(min(x,y)*s + (x+y-2*min(x,y))*w)
