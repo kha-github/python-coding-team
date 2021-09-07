f0b35e81c7d94178c7af8a2dd878324936494168
pppepp <33537862+pppepp@users.noreply.github.com>
2021-09-08 02:10:29 +0900

    Update sy_14501.py

--- 2021.09.08/14501/sy_14501.py
+++ 2021.09.08/14501/sy_14501.py
@@ -14,4 +14,12 @@ for i in range(n):
     for j in range(d, n):
         w[j] = max(w[j], w[i - 1] + P[i])
 
+# for i in range(n):
+#     d = i + T[i] - 1
+#     if w[i] == 0:
+#         w[i] = w[i - 1]
+#     if d < n:
+#         w[d] = max(w[d], w[i - 1] + P[i])
+# 왜 이렇게는 안댈가요
+        
 print(w[n - 1])
