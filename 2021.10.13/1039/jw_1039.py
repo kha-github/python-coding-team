a55a6864c9a85ed330454b30df68c198cdc1a5b4
JINWOO JEONG <62453929+jeongjinwoo96@users.noreply.github.com>
2021-10-12 01:26:21 +0900

    add jw_1039.py

--- 2021.10.13/1039/jw_1039.py
+++ 2021.10.13/1039/jw_1039.py
@@ -1,1 +1,19 @@
+n,k = map(int,input().split())
+n_len = len(str(n))
+n_str = str(n)
+num_max = 0
 
+def dfs(num,idx):
+    global num_max
+    if idx == 3:
+        num_max = max(num_max,int(num))
+    for i in range(len(num)-1):
+        for j in range(i,len(num)):
+            num = num[:i] + num[j] + num[i+1:j] + num[i] + num[j+1:]
+            dfs(num,idx+1)  
+
+if n_len == 1 or str(n)[1:] == "0"* (n_len-1):
+    print(-1)
+else:
+    dfs(n_str,0)  
+print(num_max)
