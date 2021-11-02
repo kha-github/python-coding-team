6e544490465679a53c48e8c889556c95a6168edb
JINWOO JEONG <62453929+jeongjinwoo96@users.noreply.github.com>
2021-11-02 12:51:51 +0900

    Update jw_2225.py

--- 2021.11.03/2225/jw_2225.py
+++ 2021.11.03/2225/jw_2225.py
@@ -1,1 +1,17 @@
-a
+n,k = map(int,input().split())
+M = 1000000000
+
+dp = [[0] * (k+1) for _ in range(n+1)]
+
+for i in range(1,n+1):
+    dp[i][1] = 1
+
+for i in range(1,k+1):
+    dp[1][i] = i
+
+for i in range(2,n+1):
+    for j in range(2,k+1):
+        for l in range(j):
+            dp[i][j] += dp[i-1][j-l]
+            
+print(dp[n][k] % M)
