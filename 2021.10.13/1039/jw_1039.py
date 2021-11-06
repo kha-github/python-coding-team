a1163d6f6e37b17a388bc1a8a7686a0bfa2ecee1
JINWOO JEONG <62453929+jeongjinwoo96@users.noreply.github.com>
2021-11-06 22:42:33 +0900

    Update jw_1039.py

--- 2021.10.13/1039/jw_1039.py
+++ 2021.10.13/1039/jw_1039.py
@@ -3,12 +3,11 @@ import copy
 n,k = map(int,input().split())
 n_len = len(str(n))
 n_str = str(n)
-num_max = 0
+num_max = -1
 
 def dfs(num,idx):
     global num_max
     if idx == k:
-        print(num)
         num_max = max(num_max,int(num))
         return
     for i in range(len(num)-1):
@@ -20,10 +19,9 @@ def dfs(num,idx):
             if num_list[0] == "0":
                 continue
             num_list = ''.join(num_list)
-            num_copy = copy.deepcopy(num_list)
-            dfs(num_copy,idx+1)
+            dfs(num_list,idx+1)
 
-if n_len == 1 or str(n)[1:] == "0"* (n_len-1):
+if n_len == 1:
     print(-1)
 else:
     dfs(n_str,0)
